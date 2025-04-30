7b36d1de42f4b97f21424666783120efa5b4456f
Arjunsharma5767 <157741738+Arjunsharma5767@users.noreply.github.com>
2025-04-30 21:35:24 +0530

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,22 @@
+
+workers = 1  
+
+
+worker_class = "gevent"
+
+
+timeout = 90  
+keepalive = 30  
+
+
+max_requests = 50  
+max_requests_jitter = 5  
+
+
+limit_request_line = 4094  
+limit_request_fields = 100  
+limit_request_field_size = 8190  
+
+
+capture_output = True  
+disable_redirect_access_to_syslog = True  
