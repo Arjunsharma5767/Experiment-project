a3cf3be2e7a37bb27c47f056a946679eeaeff4f1
Arjunsharma5767 <157741738+Arjunsharma5767@users.noreply.github.com>
2025-04-30 19:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Percentage Calculator</title>
+    <title> Calculator</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
